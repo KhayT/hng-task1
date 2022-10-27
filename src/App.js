f9b9090b2035587e5a9c957adcafe044c1b5cb30
KhayT <tkprof4@gmail.com>
2022-10-27 14:50:14 -0700

    completed share button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import shareIcon from "./images/share icon.svg";
+import dots from "./images/dots-horizontal.svg";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <div className="shareBtn-container">
+        <div className="tooltip">
+          <div className="content">
+            <p>Share Link</p>
+          </div>
+          <div className="bottom-center"></div>
+        </div>
+
+        <img src={shareIcon} alt="" id="share-btn" />
+
+        <img src={dots} alt="" id="dots" />
+      </div>
+    </>
+  );
 }
 
 export default App;
