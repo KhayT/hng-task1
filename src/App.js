f2bf8f2205e923c9ffc797d40ce2895fd8348b9e
KhayT <tkprof4@gmail.com>
2022-10-28 22:26:19 -0700

    styled and used the link component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Links from "./components/Links";
+
 import shareIcon from "./images/share icon.svg";
 import dots from "./images/dots-horizontal.svg";
 import img from "./images/khadijah.jpg";
@@ -30,7 +32,39 @@ function App() {
       <p id="twitter">Abdulazeez_KT</p>
       <p id="slack">Khadijah</p>
 
-      <div className="link-container"></div>
+      <div className="link-container">
+        <Links
+          url="https://twitter.com/Abdulazeez_KT"
+          title="Twitter Link"
+          id="twiter-link"
+        />
+
+        <Links
+          url="https://training.zuri.team/"
+          title="Zuri Team"
+          id="btn__zuri"
+        />
+
+        <Links url="http://books.zuri.team" title="Zuri Books" id="books" />
+
+        <Links
+          url="http://books.zuri.team"
+          title="Python Books"
+          id="book__python"
+        />
+
+        <Links
+          url="https://background.zuri.team"
+          title="Background Check for Coders"
+          id="pitch"
+        />
+
+        <Links
+          url="https://books.zuri.team/design-rules"
+          title="Design Books"
+          id="book__design"
+        />
+      </div>
     </>
   );
 }
