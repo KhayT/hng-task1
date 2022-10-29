1b38adc46e6397951557454edb096e2dd052339f
KhayT <tkprof4@gmail.com>
2022-10-28 22:22:22 -0700

    Styled profile image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import shareIcon from "./images/share icon.svg";
 import dots from "./images/dots-horizontal.svg";
+import img from "./images/khadijah.jpg";
+import cameraIcon from "./images/camera icon.svg";
 
 function App() {
   return (
@@ -15,6 +17,14 @@ function App() {
         <img src={shareIcon} alt="" id="share-btn" />
 
         <img src={dots} alt="" id="dots" />
+      </div>
+
+      <div className="image-container">
+        <img src={img} alt="profileImage" id="profile__img" />
+
+        <div className="onhover">
+          <img src={cameraIcon} alt="" id="cameraIcon" />
+        </div>
       </div>
     </>
   );
