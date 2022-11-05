d03b78fec7b348172ce83e12aa9ab32da5cff46d
KhayT <tkprof4@gmail.com>
2022-11-05 09:32:08 -0700

    style: removed input.module.css file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import classes from "./Contact.module.css";
+import Input from "./Input";
 
 const Contact = () => {
   return (
@@ -9,6 +10,51 @@ const Contact = () => {
         <p className={classes.subtext}>
           Hi there, contact me to ask me about anything you have in mind.
         </p>
+
+        <div className={classes["name-container"]}>
+          <Input
+            label="First Name"
+            type="text"
+            placeholder="Enter your first name"
+            className={classes.nameField}
+            classNameC={classes.div}
+            classNameL={classes.label}
+            classNameI={classes.input}
+            id="first name"
+          />
+
+          <Input
+            label="Last Name"
+            type="text"
+            placeholder="Enter your last name"
+            className={classes.nameField}
+            classNameC={classes.div}
+            classNameL={classes.label}
+            classNameI={classes.input}
+            id="last name"
+          />
+        </div>
+
+        <Input
+          label="Email"
+          type="email"
+          placeholder="yourname@email.com"
+          classNameC={classes.div}
+          classNameL={classes.label}
+          classNameI={classes.input}
+          id="email"
+        />
+
+        <div className={classes.div}>
+          <label htmlFor="message" className={classes.label}>
+            Message
+          </label>
+          <textarea
+            className={classes.input}
+            placeholder="Send me a message and I'll reply you as soon as possible"
+            id="message"
+          />
+        </div>
 
         <div className={classes["checkbox-container"]}>
           <input type="checkbox" name="" id="checkbox" />
---
+++ src/components/Input.js
@@ -0,0 +1,17 @@
+const Input = (props) => {
+  return (
+    <div className={`${props.className} ${props.classNameC}`}>
+      <label htmlFor={props.id} className={props.classNameL}>
+        {props.label}
+      </label>
+      <input
+        className={props.classNameI}
+        type={props.type}
+        placeholder={props.placeholder}
+        id={props.id}
+      />
+    </div>
+  );
+};
+
+export default Input;
