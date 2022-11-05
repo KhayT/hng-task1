6abdd9c303da61e801b31035c698f0b799138b5e
KhayT <tkprof4@gmail.com>
2022-11-04 19:35:19 -0700

    cleaned code

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,16 +1,7 @@
 import Contact from "../components/Contact";
 
-import Links from "../components/Link";
-
 const ContactPage = () => {
-  // return (
-  //   // <Links
-  //   //   url="https://books.zuri.team/design-rules"
-  //   //   title="Design Books"
-  //   //   subtext="Check out our available design books"
-  //   //   id="book__design"
-  //   // />
-  // );
+  return <Contact />;
 };
 
 export default ContactPage;
