4a6e7a51f238a62497a3c3e18194eae93f568ab3
KhayT <tkprof4@gmail.com>
2022-11-05 09:13:30 -0700

    style: styled contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,24 @@
 import Footer from "./Footer";
+import classes from "./Contact.module.css";
 
 const Contact = () => {
   return (
     <>
-      <h1>Contact Me</h1>
+      <div className={classes.container}>
+        <h1 className={classes.title}>Contact Me</h1>
+        <p className={classes.subtext}>
+          Hi there, contact me to ask me about anything you have in mind.
+        </p>
 
+        <div className={classes["checkbox-container"]}>
+          <input type="checkbox" name="" id="checkbox" />
+          <label htmlFor="checkbox">
+            You agree to providing your data to Khadijah who may contact you.
+          </label>
+        </div>
+
+        <button className={classes["submit-btn"]}>Send message</button>
+      </div>
       <Footer />
     </>
   );
